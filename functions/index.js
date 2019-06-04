2dfcf94986aa18b8ebaa1b5bc30e7722ae7b8489
oswaldo  martinez <oswaldinho24k@MacBook-Pro-de-oswaldo.local>
2019-06-03 23:09:49 -0500

    stand by because we finished

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
 const nodemailer = require('nodemailer')
+const admin = require('firebase-admin')
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -9,44 +9,63 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
-const gmailEmail = functions.config().gmail.mail;
-const gmailPassword = functions.config().gmail.password;
-exports.sendEmailToNewUser = functions.firestore.document('/users/{userId}').onCreate((snap, context) => {
-    
-    const val = snap.data();
-    console.log(val)
+exports.sendWelcomeMail = functions.firestore.document('/users/{userId}')
+    .onCreate((snap, context)=>{
+        console.log(snap.data())
+        const user = snap.data()
 
-    const transport = nodemailer.createTransport({
-        service:'Gmail',
-        auth:{
-            user:gmailEmail,
-            pass:gmailPassword
-        }
-    })
-    const sendMail = () => {
-        transport.sendMail({
-            from:'Oswaldinho',
-            to:val.email,
-            subject:`Welcome ${val.username}`,
-            text:'Bienvenido a la plataforma de Google planning'
+        const transport = nodemailer.createTransport({
+            service:'Gmail',
+            auth:{
+                user:functions.config().gmail.email,
+                pass:functions.config().gmail.password
+            }
         })
-        .then(r=>{
-            console.log(r)
-            return r
-        })
-        .catch(e=>{
-            console.log(e)
-            throw e
-        })
-    }
-    return sendMail()
-            .then(r=>{
+
+        const sendMail=()=>{
+            transport.sendMail({
+                from:'Oswaldinho',
+                to:user.email,
+                subject:`Welcome to the google planing app`,
+                html:`<h2>Ya eres parte 🍕🍕🍕</h2>`
+            }).then(r=>{
                 console.log(r)
                 return r
-            })
-            .catch(e=>{
+            }).catch(e=>{
                 console.log(e)
                 throw e
             })
-  
-  });
+        }
+        return sendMail()
+    })
+
+    exports.newContentMail=functions.firestore.document('/contents/{contentId}')
+        .onCreate((snap, context)=>{
+            const content = snap.data()
+            const transport = nodemailer.createTransport({
+                service:'Gmail',
+                auth:{
+                    user:functions.config().gmail.email,
+                    pass:functions.config().gmail.password
+                }
+            })
+            const sendMail=()=>{
+                transport.sendMail({
+                    from:'Oswaldinho',
+                    to:content.userEmail,
+                    subject:`You created a new content`,
+                    html:`<h2>You created ${content.text}</h2>
+                        <img src="${content.imageURL}"/>    
+                    `
+                }).then(r=>{
+                    console.log(r)
+                    return r
+                }).catch(e=>{
+                    console.log(e)
+                    throw e
+                })
+            }
+
+            return sendMail()
+        })
+
